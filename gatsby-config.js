91ccf9ecf6e5a731ca507d552f750e77f5982150
Evan Martin <evan@catalystlab.ai>
2020-03-30 16:56:18 +0100

    Netlify CMS implementation

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        enableIdentityWidget: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
