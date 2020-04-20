bf8d2821716d21d66f2a99826e5d4fa963f61f15
Jazzy Gasper <jasmina.gasper@gmail.com>
2020-04-20 16:44:48 +0100

    button styles

--- src/components/donate/DonationAmount/DonationAmount.tsx
+++ src/components/donate/DonationAmount/DonationAmount.tsx
@@ -10,32 +10,52 @@ class DonationAmount extends React.Component<Props, State> {
   state = {
     amount: 0
   }
+
   handleAmountChange = (e) => {
-    this.setState({customAmount: 0 })
-    this.props.onChange(e.target.dataset['amount']);
+    const amount = parseInt(e.target.dataset['amount'])
+    this.setState({customAmount: 0, amount: amount})
+    this.props.onChange(amount)
   };
 
   handleCustomAmountChange = (e) => {
     const amount = e.target.value
-    this.setState({customAmount: amount})
-    this.props.onChange(amount);
+    this.setState({amount: 0, customAmount: amount})
+    this.props.onChange(amount)
   };
+
+  renderButton = (amount, i) => {
+    console.log('amount', amount)
+    console.log(this.state.amount)
+    let classNames = styles.button
+    if (amount === this.state.amount) {
+      classNames = `${styles.button} ${styles.selected}`
+    }
+    return <button onClick={this.handleAmountChange} data-amount={amount} className={classNames} key={i}>£{amount}</button>
+  }
+
+  renderCustomAmount = (customAmount) => {
+    let classNames = styles.custom
+    if (customAmount > 0) {
+      classNames = `${styles.custom} ${styles.custom_selected}`
+    }
+    return (<div className={classNames}>
+      <span>£</span>
+      <input
+        type="number"
+        value={customAmount > 0 ? customAmount : ''}
+        placeholder="Enter amount"
+        onChange={this.handleCustomAmountChange}
+      />
+    </div>)
+  }
 
 
     render() {
       return (
         <div className={styles.buttons}>
-          {DONATION_AMOUNTS.map((object, i) => <button onClick={this.handleAmountChange} data-amount={object} className={styles.button} key={i}>£{object}</button>)}
-          <div className={styles.custom}>
-            <span>£</span>
-            <input
-              type="number"
-              value={this.state.customAmount > 0 ? this.state.customAmount : ''}
-              placeholder="Enter amount"
-              onChange={this.handleCustomAmountChange}
-            />
-          </div>
-      </div>
+          {DONATION_AMOUNTS.map((amount, i) => this.renderButton(amount, i))}
+          {this.renderCustomAmount(this.state.customAmount)}
+        </div>
       )
     }
   }
