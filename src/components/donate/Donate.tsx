0d361081e818d06fb2fb658a8f31bbbdc03922a1
Tom Waddington <tom@tomwaddington.co.uk>
2020-04-21 15:38:55 +0100

    Donation API

--- src/components/donate/Donate.tsx
+++ src/components/donate/Donate.tsx
@@ -11,20 +11,43 @@ class Donate extends React.Component<Props, State> {
 
   state = {
     name: '',
-    email: ''
+    email: '',
+    termsAndConditions: false,
+    amount: 0
   }
 
-  handleChangeAmount = (amount) => {
-    console.log(amount)
+  handleChangeAmount = (amount: number) => {
     this.setState({ amount });
   }
 
-  handleChangeDetails = ({ name, email }) => {
-    this.setState({ name: name, email: email });
+  handleChangeDetails = ({ name, email, termsAndConditions }) => {
+    this.setState({ name: name, email: email, termsAndConditions: termsAndConditions });
   }
 
   isValid = () => {
-    return (this.state.amount > 0) && this.state.name
+    return (this.state.amount > 0) && this.state.name && this.state.email && this.state.termsAndConditions
+  }
+
+  handleBankedPayment = () => {
+    const { name, email, termsAndConditions, amount } = this.state
+
+    fetch("https://europe-west2-banked-heroes-dev.cloudfunctions.net/createBankedPaymentRequest", {
+      accept: 'application/json',
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        amount: amount * 100,
+        payerName: name,
+        payerEmail: email,
+      }),
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      window.location.href = response.url;
+    })
   }
 
   render() {
@@ -32,25 +55,22 @@ class Donate extends React.Component<Props, State> {
       [sharedStyles.button]: true,
       [sharedStyles.buttonEnabled]: this.isValid()
     })
-    const {
-        name, email
-      } = this.state;
-      
+
     return (
       <div className="container">
         <h2>How much would you like to donate?</h2>
           <DonationAmount onChange={this.handleChangeAmount} />
 
         <h2>Enter your details</h2>
-          <DonationDetails name={name} email={email} onChange={this.handleChangeDetails} />
+          <DonationDetails onChange={this.handleChangeDetails} />
 
         <h2>How would you like to pay?</h2>
         <p>Select a payment method to make your donation securely.</p>
 
         <p>If you are making a large donation - please consider using ‘Bank Account’ to reduce our fees </p>
 
-        <button className={buttonClassNames}>Pay by card</button>
-        <button className={buttonClassNames}>Pay by bank account</button>
+        {/* <button className={buttonClassNames}>Pay by card</button> */}
+        <button onClick={this.handleBankedPayment} className={buttonClassNames}>Pay by bank account</button>
       </div>
     )
   }
--- src/components/donate/DonationDetails/DonationDetails.tsx
+++ src/components/donate/DonationDetails/DonationDetails.tsx
@@ -4,51 +4,88 @@ import * as styles from "./DonationDetails.module.scss"
 
 class DonationDetails extends React.Component<Props, State> {
 
-  handleNameChange = (e) => {
-    this.props.onChange({ name: e.target.value });
+  state = {
+    name: '',
+    email: '',
+    termsAndConditions: false
+  }
+
+  handleNameChange = (e: Event) => {
+    this.setState({ name: e.target.value });
   };
 
-  handleEmailChange = (e) => {
-    this.props.onChange({ email: e.target.value });
+  handleEmailChange = (e: Event) => {
+    this.setState({ email: e.target.value });
   };
-    
-    render() {
-      return (
-        <div>
-            <div className={styles.details__fields}>
-            <div className="your-details__field">
-              <label className="your-details__field__label" htmlFor="payment_session_user_name">Full Name</label>
-              <input
-                required
-                ref={this.nameInput}
-                type="text"
-                className="your-details__field__input"
-                placeholder="Enter your full name"
-                onChange={this.handleNameChange}
-                value={this.props.name}
-                autoComplete="name"
-                id="payment_session_user_name"
-              />
-            </div>
-            <div className="your-details__field">
-              <label className="your-details__field__label" htmlFor="payment_session_email">Email Address</label>
-              <input
-                required
-                ref={this.emailInput}
-                type="email"
-                className="your-details__field__input"
-                placeholder="Enter your email address"
-                onChange={this.handleEmailChange}
-                value={this.props.email}
-                autoComplete="email"
-                id="payment_session_email"
-              />
-            </div>
-          </div>
-        </div>
-      )
+
+  handleTermsAndConditionsChange = (e: Event) => {
+    this.setState({ termsAndConditions: e.target.checked });
+  };
+
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.name != this.state.name || prevState.email != this.state.email || prevState.termsAndConditions != this.state.termsAndConditions) {
+      this.props.onChange({
+        name: this.state.name,
+        email: this.state.email,
+        termsAndConditions: this.state.termsAndConditions
+      });
     }
   }
-  
 
-  export default DonationDetails
+  render() {
+    const {
+      name, email, termsAndConditions
+    } = this.state;
+    return (
+      <div>
+          <div className={styles.details__fields}>
+          <div>
+            <label className={styles.inputLabel} htmlFor="payment_session_user_name">Full Name</label>
+            <input
+              required
+              ref={this.nameInput}
+              className={styles.input}
+              type="text"
+              placeholder="Enter your full name"
+              onChange={this.handleNameChange}
+              value={name}
+              autoComplete="name"
+              id="payment_session_user_name"
+            />
+          </div>
+          <div>
+            <label className={styles.inputLabel} htmlFor="payment_session_email">Email Address</label>
+            <input
+              required
+              ref={this.emailInput}
+              className={styles.input}
+              type="email"
+              placeholder="Enter your email address"
+              onChange={this.handleEmailChange}
+              value={email}
+              autoComplete="email"
+              id="payment_session_email"
+            />
+          </div>
+
+          <div className={styles.termsAndConditions}>
+            <label htmlFor="payment_session_terms_and_conditions">
+            <input
+              required
+              ref={this.termsAndConditions}
+              type="checkbox"
+              onChange={this.handleTermsAndConditionsChange}
+              value={termsAndConditions}
+              id="payment_session_terms_and_conditions"
+            />
+            I have read and accept the <a href="#">terms &amp; conditions</a>
+            </label>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+  
+export default DonationDetails
