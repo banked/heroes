50646338f36f876d01691ce57ac5caa1878d0c01
Tom Waddington <tom@tomwaddington.co.uk>
2020-04-21 15:38:55 +0100

    Donation API

--- src/components/donate/DonationAmount/DonationAmount.tsx
+++ src/components/donate/DonationAmount/DonationAmount.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import classNames from 'classnames';
 
 import * as styles from "./DonationAmount.module.scss"
 
@@ -8,57 +9,64 @@ const DONATION_AMOUNTS = [5,10,20,50,100,250]
 class DonationAmount extends React.Component<Props, State> {
 
   state = {
-    amount: 0
+    amount: 0,
+    customAmount: null
   }
 
-  handleAmountChange = (e) => {
+  handleAmountChange = (e: Event) => {
     const amount = parseInt(e.target.dataset['amount'])
-    this.setState({customAmount: 0, amount: amount})
+    this.setState({customAmount: null, amount: amount})
     this.props.onChange(amount)
   };
 
-  handleCustomAmountChange = (e) => {
+  handleCustomAmountChange = (e: Event) => {
     const amount = e.target.value
     this.setState({amount: 0, customAmount: amount})
     this.props.onChange(amount)
   };
 
-  renderButton = (amount, i) => {
-    console.log('amount', amount)
-    console.log(this.state.amount)
-    let classNames = styles.button
-    if (amount === this.state.amount) {
-      classNames = `${styles.button} ${styles.selected}`
-    }
-    return <button onClick={this.handleAmountChange} data-amount={amount} className={classNames} key={i}>£{amount}</button>
+  initCustomAmount = () => {
+    this.setState({amount: 0, customAmount: 0})
   }
 
-  renderCustomAmount = (customAmount) => {
-    let classNames = styles.custom
-    if (customAmount > 0) {
-      classNames = `${styles.custom} ${styles.custom_selected}`
-    }
-    return (<div className={classNames}>
+  renderButton = (amount: number, i) => {
+    const buttonClassNames = classNames({
+      [styles.button]: true,
+      [styles.selected]: (amount === this.state.amount)
+    })
+
+    return <button onClick={this.handleAmountChange} data-amount={amount} className={buttonClassNames} key={i}>£{amount}</button>
+  }
+
+  renderCustomAmount = (customAmount: number) => {
+    const divClassNames = classNames({
+      [styles.custom]: true,
+      [styles.custom_selected]: (customAmount !== null)
+    })
+
+    return (<div className={divClassNames}>
       <span>£</span>
       <input
         type="number"
-        value={customAmount > 0 ? customAmount : ''}
+        value={customAmount || ''}
         placeholder="Enter amount"
         onChange={this.handleCustomAmountChange}
+        onFocus={this.initCustomAmount}
+        min="0"
+        max="1000"
+        step="0.01"
       />
     </div>)
   }
 
-
-    render() {
-      return (
-        <div className={styles.buttons}>
-          {DONATION_AMOUNTS.map((amount, i) => this.renderButton(amount, i))}
-          {this.renderCustomAmount(this.state.customAmount)}
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div className={styles.buttons}>
+        {DONATION_AMOUNTS.map((amount, i) => this.renderButton(amount, i))}
+        {this.renderCustomAmount(this.state.customAmount)}
+      </div>
+    )
   }
+}
 
-
-  export default DonationAmount
+export default DonationAmount
