1f6f67990a765f90379b58e18a2a8ef25616ce22
Jazzy Gasper <jasmina.gasper@gmail.com>
2020-04-22 17:05:21 +0100

    success and error pages

--- src/pages/donate/error.tsx
+++ src/pages/donate/error.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../../components/donate/Layout"
 
@@ -13,7 +13,13 @@ const DonateSuccessPage = () => {
 
     return (
       <Layout>
-        <h2>Payment Failed</h2>
+        <div className={styles.layout}>
+          <h2>Payment Failed</h2>
+          <p>Unfortunately we were not able to proccess your donation.</p>
+          <p>Please try again.</p>
+
+          <Link className={styles.button} to='/donate' >Go Back</Link>
+        </div>
       </Layout>
     )
 
