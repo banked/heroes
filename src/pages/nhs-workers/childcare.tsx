96f68dbdd406c34cc315e8532949d1ee27dcc0de
Evan Martin <evan@catalystlab.ai>
2020-04-27 12:16:51 +0100

    Childcare changes

--- src/pages/nhs-workers/childcare.tsx
+++ src/pages/nhs-workers/childcare.tsx
@@ -7,18 +7,45 @@ import ComingSoonPage from "../../components/shared/ComingSoon"
 import { Details } from "../../components/shared/ComingSoon/ComingSoon"
 
 import childcare from "../../images/forNHSWorkers/childcare.png"
+import { Link } from "gatsby"
 
 const details: Details = {
   title: "Childcare",
   byline:
     "HEROES have collaborated with the country’s largest childcare provider, childcare.co.uk, to provide free at the point of use childcare services for NHS workers, and to provide the ability for NHS workers to co-op and provide childcare for each other if possible.",
-  applyFor: "Childcare Grant",
-  applyByline:
-    "To help cover the costs, HEROES members can apply for a small grant up to £1500 specifically for childcare. To find out more visit please visit the grants page.",
+  applyFor: "Childcare Support",
+  applyByline: "",
   icon: childcare,
-  basicContent: {
-    website: "/grants-and-financial-assistance",
-  },
+  customContent: (
+    <>
+      <div>
+        <span>
+          To help cover the costs, HEROES members can apply for a small grant up
+          to £1500 specifically for childcare. To find out more visit please
+          visit the grants page.
+        </span>
+        <Link to="/grants-and-financial-assistance">
+          <button>Grants Page</button>
+        </Link>
+      </div>
+      <div>
+        <span>
+          Access childcare services through{" "}
+          <a
+            href="http://childcare.co.uk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            childcare.co.uk
+          </a>
+          . Details about this exciting collaboration will be announced shortly.
+        </span>
+        <a href="#">
+          <button>Coming Soon</button>
+        </a>
+      </div>
+    </>
+  ),
 }
 
 const GrantsAndFinancialAidPage = () => {
